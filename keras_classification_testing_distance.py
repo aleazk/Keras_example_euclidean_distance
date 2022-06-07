8b618da6f4394c6095d700ffce245c96a6d8d377
Alejandro Ascárate <106303166+aleazk@users.noreply.github.com>
2022-06-07 15:38:15 -0300

    Update keras_classification_testing_distance.py

--- keras_classification_testing_distance.py
+++ keras_classification_testing_distance.py
@@ -1,11 +1,9 @@
 import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
+
 
 import keras 
 
-from keras.models import Sequential
-from keras.layers import Dense
+
 from keras.utils import to_categorical
 
 X_train, y_train = np.loadtxt("X_train.csv", delimiter=","), np.loadtxt("y_train.csv", delimiter=",")
