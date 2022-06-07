c4f8c254fdd4f583b9b45014146ffd2ff014ca14
Alejandro Ascárate <106303166+aleazk@users.noreply.github.com>
2022-06-07 15:49:40 -0300

    Update training&test_data_generator_using_distance_function.py

--- training&test_data_generator_using_distance_function.py
+++ training&test_data_generator_using_distance_function.py
@@ -4,10 +4,7 @@ X_train = np.random.randint(0, 10, size=(10000, 10))
 
 X_test = np.random.randint(0, 10, size=(100, 10))
 
-y_train = np.array([1 if np.sqrt((X_train*X_train).sum(axis=1))[i]<=15 else 0 for i in range(0, 10000)]) #use various other values besides 15, since,
-#in that way, the same data set can be the input for different concepts (captured by the values of y_train/test) which may both be still true if, say, for d=15 is true and, then, if the
-#other concept has d=20, it will also be true. In particular, this is an implication (it can also be used to validate the conjuction of the concepts involved).
-
+y_train = np.array([1 if np.sqrt((X_train*X_train).sum(axis=1))[i]<=15 else 0 for i in range(0, 10000)])
 y_test = np.array([1 if np.sqrt((X_test*X_test).sum(axis=1))[i]<=15 else 0 for i in range(0, 100)])
 
 np.savetxt("X_train.csv", X_train, delimiter=",")
